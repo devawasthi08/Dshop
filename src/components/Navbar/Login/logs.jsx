27c08cefda0b0f0dc6ec0a1d624ae9164180ea72
devawasthi08 <devanshawasthi0502314@gmail.com>
2025-08-22 01:06:44 +0530

    f

---
+++ src/components/Navbar/Login/logs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logs = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Login Logs</h1>
+      <p>This page will show logs later.</p>
+    </div>
+  );
+};
+
+export default Logs;
